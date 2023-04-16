cb0c1e0915b308ecd484e2717b969ee460baf930
heidi-chiehanlin <chiehan.linn@gmail.com>
2023-04-16 12:36:02 -0700

    add features to BankAccount

--- BankAccount.py
+++ BankAccount.py
@@ -1,6 +1,17 @@
 class BankAccount:
-    def __init__(self, acct_no: str) -> None:
+    def __init__(self, acct_no, name='unknown', currency='unknown') -> None:
         self._acct_no = acct_no
+        self._holder_name = name
+        self._currency = currency
 
     def get_acct_no(self) -> str:
         return self._acct_no
+
+    def get_holder_name(self) -> str:
+        return self._holder_name
+
+    def get_currency(self) -> str:
+        return self._currency
+
+    def is_valid_acct(self) -> bool:
+        return self._holder_name != 'unknown' and self._currency != 'unknown'
--- test.py
+++ test.py
@@ -4,3 +4,23 @@ from BankAccount import BankAccount
 def test_get_acct_no():
     bank_acct = BankAccount('20230416')
     assert bank_acct.get_acct_no() == '20230416'
+
+
+def test_get_holder_name():
+    bank_acct = BankAccount('20230416', 'Amy', 'USD')
+    assert bank_acct.get_holder_name() == 'Amy'
+
+
+def test_get_currency():
+    bank_acct = BankAccount('20230416', 'Amy', 'USD')
+    assert bank_acct.get_currency() == 'USD'
+
+
+def test_unvalid_acct():
+    bank_acct = BankAccount('20230416', 'Amy')
+    assert bank_acct.is_valid_acct() == False
+
+
+def test_valid_acct():
+    bank_acct = BankAccount('20230416', 'Amy', 'USD')
+    assert bank_acct.is_valid_acct() == True
